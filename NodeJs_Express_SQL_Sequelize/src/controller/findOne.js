4febe7898aeab7a335c5420a4fdc23b8ae5fa3f7
leonardopadilha <leonardopadilha28@gmail.com>
2023-11-20 05:53:48 -0300

    Pesquisando apenas um cliente

--- NodeJs_Express_SQL_Sequelize/src/controller/findOne.js
+++ NodeJs_Express_SQL_Sequelize/src/controller/findOne.js
@@ -1,15 +1,20 @@
 const { Sequelize } = require('sequelize')
-let sequelize = require('../model/index')
-const path = require('path');
+let sequelize = require('./../model/index')
+const path = require('path')
 let Cliente = require(path.join(__dirname, './../model/client'))(sequelize, Sequelize.DataTypes)
 
 module.exports = (req, res) => {
     Cliente
-        .findAll()
-        .then((clients) => {
-            console.log(clients)
+        .findOne({
+            where: {
+                id: req.params.id
+            }
+        }).then((client) => {
+            console.log(client)
         })
         .catch((err) => {
             console.log(err)
         })
 }
+
+
--- NodeJs_Express_SQL_Sequelize/src/routes/client.js
+++ NodeJs_Express_SQL_Sequelize/src/routes/client.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router() 
 
+let findOne = require('./../controller/findOne')
 let findAll = require('./../controller/findAll') 
 let create = require('./../controller/create')
 let update = require('./../controller/update')
 let remove = require('./../controller/remove')
 
+router.get('/:id', findOne)
 router.get('/', findAll)
 
 /* router.get('/welcome', (req, res) => {
